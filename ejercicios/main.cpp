ae2975a3a9f7439b3eceef69913a07c84821a873
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-24 12:47:10 -0500

    segunda clase

---
+++ ejercicios/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    /*cout << "ingrese el radio del circulo" << endl;
+    float pi=3.14;
+    float A, Radio;
+    cin>>Radio;
+    A=pi*Radio*Radio;
+     cout<<"El area del circulo es "<<A<<endl;
+
+
+    cout << "ingrese la base del rectangulo" << endl;
+    float Base;
+    cin >> Base;
+    cout << "ingrese la altura" << endl;
+    float Altura, AC;
+    cin >> Altura;
+    AC= Base*Altura;
+    cout <<" El area del rectangulo es " <<AC<< endl;
+
+
+    cout << "ingrese el lado del cuadrado" << endl;
+    float lado;
+    cin >> lado;
+    float ACU;
+    ACU = lado*lado;
+    cout << " El area del cuadrado es"<< ACU<<endl;
+
+
+
+        int a=6,b=3,tem;
+        tem=a;
+        a=b;
+        b=tem;
+        cout<<"Ahora el valor de A es: "<<
+        a<<" y el valor de B es: "<<b;
+        return 0;*/
+
+        int num1,num2;
+        cout<<"Ingresa primer numero "<<endl;
+        cin>>num1;
+        cout<<"Ingrese segundo numero "<<endl;
+        cin>>num2;
+        if (num1>num2)
+            cout <<"el numero mayor es:"<< num1<<endl;
+        else
+            cout <<"el numero mayor es : "<<num2<<endl;
+
+
+
+
+
+}
