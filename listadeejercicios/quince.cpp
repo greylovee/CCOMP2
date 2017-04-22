2006bde9c629e9aa984d2795539df57d43d11b71
greylovee <greylovee@gmail.com>
2017-04-21 19:07:03 -0500

    Add files via upload

---
+++ listadeejercicios/quince.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+    int main(){
+        string palabra;
+        cout<<"escribe una palabra"<<endl;
+        cin>>palabra;
+
+        int i,tam;
+
+        string alreves;
+        tam=palabra.length();
+        for(i=tam-1;1>=0;1--)  {
+            cout<<palabra[i];
+
+}
+    }
