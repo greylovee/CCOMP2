ea8ac30589053903dffe5e1fff6d1b1163dcd88e
greylovee <greylovee@gmail.com>
2017-05-25 15:45:43 -0500

    Add files via upload

---
+++ polimorfismo/include/persona.h
@@ -0,0 +1,14 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+
+class persona
+{
+    public:
+        persona();
+        virtual ~persona();
+    protected:
+    private:
+};
+
+#endif // PERSONA_H
---
+++ polimorfismo/src/persona.cpp
@@ -0,0 +1,11 @@
+#include "persona.h"
+
+persona::persona()
+{
+    //ctor
+}
+
+persona::~persona()
+{
+    //dtor
+}
