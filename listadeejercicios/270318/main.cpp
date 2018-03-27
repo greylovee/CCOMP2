15965d8d215beb99a9e6c75f2a32671ba2066570
greylovee <greylovee@gmail.com>
2018-03-27 11:25:58 -0500

    Add files via upload

---
+++ listadeejercicios/270318/main.cpp
@@ -0,0 +1,169 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+
+  /*int edad;
+  cout<<" Que edad tienes...?";
+  cin>>edad;
+  if ( edad>=18){
+
+    cout<<" PUEDES SALIR ";}
+
+
+  else {
+
+    cout << "AUN ERES UN ENANO";}
+
+    // escribir un programa que me pida 3 numeros y de la suma, producto, el promedio, el mayor y menor
+     int a,b,c;
+     int suma=0;
+     int producto = 0;
+     int promedio =0;
+
+    cout<<"ingrese el primer numero ";
+    cin>>a;
+    cout<<"ingrese el segundo numero ";
+    cin>>b;
+    cout<<"ingrese el tercer numero ";
+    cin>>c;
+
+    suma= a+b+c;
+    producto= a*b*c;
+    promedio = suma/3;
+    cout <<" la suma es "<<suma<<endl;
+    cout <<" el producto es "<< producto<<endl;
+    cout <<" el promedio es "<< promedio<<endl;
+
+    if( a > b && a > c){
+        cout << " el mayor es "<<a<<endl;
+        }
+    if (b> a && b > c){
+        cout << " el mayor es "<<b<<endl;
+        }
+    if (c>a && c>b){
+        cout << " el mayor es " << c<<endl;
+        }
+    if ( a< b && a< c){
+        cout << "el menor es "<<a<<endl;
+        }
+    if ( b< a && b< c){
+        cout << "el menor es "<<b<<endl;
+        }
+    if ( c< a && c< b){
+        cout << "el menor es "<<c<<endl;
+}
+ // escribir un programa y que pida 5 numeros y decir cual es el mayor y menor
+     int a,b,c,d,e;
+     cout<<"ingrese el primer numero ";
+        cin>>a;
+    cout<<"ingrese el segundo numero ";
+        cin>>b;
+    cout<<"ingrese el tercer numero ";
+        cin>>c;
+    cout<<"ingrese el cuarto numero ";
+        cin>>d;
+    cout<<"ingrese el quinto numero  ";
+        cin>>e;
+
+    if( a > b && a > c && a> d && a> e){
+        cout << " el mayor es "<<a<<endl;
+        }
+    if (b > a && b > c && b> d && b> e){
+        cout << " el mayor es "<<b<<endl;
+        }
+    if (c > a && c > b && c> d && c> e){
+        cout << " el mayor es " << c<<endl;
+        }
+    if (d > a && d > b && d> c && d> e){
+       cout << "el mayor es "<<d<<endl;
+        }
+    if (e > a && e > b && e> c && e> d){
+        cout << "el mayor es "<<e<<endl;
+        }
+    if (a < b && a < c && a< d && a< e){
+        cout << "el menor es "<<a<<endl;}
+    if( b< b && b< c&& b< d&& b<e){
+        cout << "el menor es"<< b<<endl;}
+     if( c< a && c< b&& c< d&& c<e){
+        cout << "el menor es"<< c<<endl;}
+     if( d< a && d< b&& d< c&& d<e){
+        cout << "el menor es"<< d<<endl;}
+         if( e< a && e< b&& e< c&& e<d){
+        cout << "el menor es"<< e<<endl;}
+
+//indicar si un numero es par
+ int num;
+ cout<<"ingresar numero ";
+ cin>>num;
+ if (num%2){
+    cout<<"es impar"<<endl;
+ }
+ else{
+    cout<<"es par"<<endl;}
+
+// implementar un programa y que diga si el primero es multiplo del segundo
+int num ,num1;
+cout<<"ingrese ambos numeros  ";
+cin>>num;
+cin>>num1;
+if ( num % num1 ==0){
+    cout << "es multiplo";
+}
+else{
+    cout <<"no es multiplo";
+}*/
+
+//ingresar numero de 5 digitos y q imprima cada numero por separado
+
+int numero, num;
+cout<<"ingrese un numero de 5 digitos   ";
+cin>>numero;
+
+num= numero/10000;
+cout<<num;
+num=numero/1000;
+cout<<num;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
